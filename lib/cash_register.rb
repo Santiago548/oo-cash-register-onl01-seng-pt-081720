707eda52c5a345345230e7327238c3390d00f111
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-12 00:44:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
+  attr_accessor :total, :discount
 
+  def initialize(discount = 20)
+    @total = 0
+    @discount
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(title, price)
+
+  end
+end
